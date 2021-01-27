26abe52918aa1483abee1ca296d999aec26f6b8c
tramnewin <trmnguyen2608@gmail.com>
2021-01-27 14:17:46 -0700

    second commit

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -8,7 +8,14 @@ module.exports = async (phrase) => {
         }
     })
 
-    return JSON.stringify(results.data);
+    let customObject = {
+        data: results.data,
+        status: results.status,
+        statusText: results.statusText,
+        headers: results.headers,
+        requestHeader: results.config.headers
+    }
+    return JSON.stringify(customObject);
 }
 
 /*  console.log(results.data);
